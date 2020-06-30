d0786ee65db038ae941bca606be5e3774d9a097a
Tejas <tejas.krishnan81@gmail.com>
2020-06-30 12:25:18 +0200

    Initial Commit

---
+++ SchedulingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SchedulingApp
+//
+//  Created by Tejas Krishnan on 6/30/20.
+//  Copyright © 2020 Tejas Krishnan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

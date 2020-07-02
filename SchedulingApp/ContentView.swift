eea559a48b110c4a7f9e1bfb9a81ea49f13b4b8e
Tejas Krishnan <tejas.krishnan81@gmail.com>
2020-07-02 17:06:08 +0200

    cool

--- SchedulingApp/ContentView.swift
+++ SchedulingApp/ContentView.swift
@@ -22,7 +22,7 @@ struct ContentView: View {
             }.tag(1)
             ClassesView().tabItem {
                 Image(systemName: "list.dash")
-                Text("Classes")
+                Text("Classes").font(.title)
                 
             }.tag(1)
              FilterView().tabItem {

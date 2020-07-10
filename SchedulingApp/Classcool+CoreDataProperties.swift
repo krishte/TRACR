d4ea61b90bd661d46fa014a67c2b043b7377efeb
Tejas Krishnan <tejas.krishnan81@gmail.com>
2020-07-10 15:52:52 +0200

    DataModel working

---
+++ SchedulingApp/Classcool+CoreDataProperties.swift
@@ -0,0 +1,24 @@
+//
+//  Classcool+CoreDataProperties.swift
+//  SchedulingApp
+//
+//  Created by Tejas Krishnan on 7/10/20.
+//  Copyright © 2020 Tejas Krishnan. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Classcool {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Classcool> {
+        return NSFetchRequest<Classcool>(entityName: "Classcool")
+    }
+
+    @NSManaged public var attentionspan: Int64
+    @NSManaged public var name: String
+    @NSManaged public var tolerance: Int64
+
+}
--- SchedulingApp/ClassesView.swift
+++ SchedulingApp/ClassesView.swift
@@ -8,60 +8,71 @@
 
 import SwiftUI
 
-class Classcool: Identifiable {
-    var name: String = ""
-    var attentionspan: Int = 0
-    var tolerance: Int = 0
-    var color: Color = Color.blue
-    var assignmentlist: [Assignment] = []
-    
-    init(name: String, attentionspan: Int, tolerance: Int, color: Color, assignmentlist: [Assignment])
-    {
-        self.name = name
-        self.attentionspan = attentionspan
-        self.tolerance = tolerance
-        self.color = color
-        self.assignmentlist = assignmentlist
-    }
-}
+//class Classcool: Identifiable {
+//    var name: String = ""
+//    var attentionspan: Int = 0
+//    var tolerance: Int = 0
+//    var color: Color = Color.blue
+//    var assignmentlist: [Assignment] = []
+//
+//    init(name: String, attentionspan: Int, tolerance: Int, color: Color, assignmentlist: [Assignment])
+//    {
+//        self.name = name
+//        self.attentionspan = attentionspan
+//        self.tolerance = tolerance
+//        self.color = color
+//        self.assignmentlist = assignmentlist
+//    }
+//}
 
-class Assignment: Identifiable {
-    var subject: String = ""
-    var name: String = ""
-    var type: String = ""
-    var duedate: String = ""
-    var totaltime: Int = 0
-    var progress: Int = 0
-    var timeleft: Int = 0
-    var subassigmentlist: [SubAssignment] = []
-    
-    
-    init(subject: String, name: String, type: String, duedate: String, totaltime: Int, progress: Int, timeleft: Int, subsylist: [SubAssignment])
-    {
-        self.subject = subject
-        self.name = name
-        self.type = type
-        self.duedate = duedate
-        self.totaltime = totaltime
-        self.progress = progress
-        self.timeleft = timeleft
-        self.subassigmentlist = subsylist
-        
-    }
-    
-}
+//class Assignment: Identifiable {
+//    var subject: String = ""
+//    var name: String = ""
+//    var type: AssignmentTypes = AssignmentTypes.exam
+//    var duedate: Date
+//    var totaltime: Int = 0
+//    var progress: Int = 0
+//    var timeleft: Int = 0
+//    var subassigmentlist: [SubAssignment] = []
+//
+//
+//    init(subject: String, name: String, type: AssignmentTypes, duedate: Date, totaltime: Int, progress: Int, timeleft: Int, subsylist: [SubAssignment])
+//    {
+//        self.subject = subject
+//        self.name = name
+//        self.type = type
+//        self.duedate = duedate
+//        self.totaltime = totaltime
+//        self.progress = progress
+//        self.timeleft = timeleft
+//        self.subassigmentlist = subsylist
+//
+//    }
+//
+//}
+
+//enum AssignmentTypes: String {
+//    case essay
+//    case exam
+//    case presentation
+//    case test
+//}
 
 class SubAssignment: Identifiable {
     var startdatetime: String = ""
     var enddatetime: String = ""
+    var assignmentname: String = ""
     
-    init(startdatetime: String, enddatetime: String)
+    init(startdatetime: String, enddatetime: String, assignmentname: String)
     {
         self.startdatetime = startdatetime
         self.enddatetime = enddatetime
+        self.assignmentname = assignmentname
+        
     }
 
 }
+
 
 
 struct ClassView: View {
@@ -72,11 +83,11 @@ struct ClassView: View {
         HStack {
             VStack(alignment: .leading) {
                 
-                Text(classcool.name).foregroundColor(classcool.color)
+                Text(classcool.name)
                 
             }
             Spacer()
-            Text(String(classcool.assignmentlist.count))
+//            Text(String(classcool.assignmentlist.count))
         }
     }
 }
@@ -86,7 +97,7 @@ struct DetailView: View {
     
     var body: some View {
         VStack {
-            Text(classcool.name).font(.title).foregroundColor(classcool.color)
+            Text(classcool.name).font(.title)
             
             Text("Tolerance: " + String(classcool.tolerance))
             
@@ -98,21 +109,29 @@ struct DetailView: View {
 struct ClassesView: View {
 
     
-    var classlist = [
-        Classcool(name: "German", attentionspan: 5, tolerance: 4, color: .blue, assignmentlist: []),
-        Classcool(name: "Math", attentionspan: 4, tolerance: 3,color: .green, assignmentlist: []),
-        Classcool(name: "English", attentionspan: 1, tolerance: 2,color: .orange, assignmentlist: [])
-           
-        
+    @Environment(\.managedObjectContext) var managedObjectContext
     
-    ]
+    @FetchRequest(entity: Classcool.entity(),
+                  sortDescriptors: [])
+    
+    var classlist: FetchedResults<Classcool>
+//
+//    var classlist: [Classcool] = [
+////        Classcool(name: "German", attentionspan: 5, tolerance: 4, color: .blue, assignmentlist: []),
+////        Classcool(name: "Math", attentionspan: 4, tolerance: 3,color: .green, assignmentlist: []),
+////        Classcool(name: "English", attentionspan: 1, tolerance: 2,color: .orange, assignmentlist: [])
+////
+//
+//
+//    ]
     var globalassignmentlist: [Assignment] = []
     
     var body: some View {
+
      NavigationView{
           List(classlist) { classcool in
-                NavigationLink(destination: DetailView(classcool: classcool)) {
-                    ClassView(classcool:classcool)
+            NavigationLink(destination: DetailView(classcool: classcool )) {
+                ClassView(classcool:classcool )
                 }
           }
         
@@ -120,11 +139,25 @@ struct ClassesView: View {
              HStack {
              Button(action: {}) {
                 Image(systemName: "gear").resizable().scaledToFit().font(.title)
-             }.foregroundColor(.black)
+                }.foregroundColor(.black)
               
             },trailing:
          HStack {
-             Button(action: {}) {
+             Button(action: {
+        
+                let newClass = Classcool(context: self.managedObjectContext)
+                newClass.attentionspan = 5
+                newClass.tolerance = 7
+                newClass.name = "Math"
+                      print("hello")
+                do {
+                 try self.managedObjectContext.save()
+                 print("Order saved.")
+                } catch {
+                 print(error.localizedDescription)
+                 }
+                
+             }) {
                  Image(systemName: "plus")
                     .font(.title)
              }.foregroundColor(.black)
@@ -136,6 +169,9 @@ struct ClassesView: View {
 
 struct ClassesView_Previews: PreviewProvider {
     static var previews: some View {
-        ClassesView()
+      let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        return ClassesView().environment(\.managedObjectContext, context)
+
     }
 }

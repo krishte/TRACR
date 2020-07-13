c826d69e6092a0352cec826dbbe50dea293be787
vadrech <charan.vadrevu00@gmail.com>
2020-07-13 22:10:14 +0200

    styling for class list and color palette change

--- SchedulingApp/HomeView.swift
+++ SchedulingApp/HomeView.swift
@@ -90,7 +90,7 @@ struct HomeView: View {
                      .navigationBarItems(
                         leading:
                             HStack(spacing: geometry.size.width / 4.2) {
-                                Button(action: {print("settings button clicked")}) {
+                                Button(action: {print("settings button clickedd")}) {
                                     Image(systemName: "gear").renderingMode(.original).resizable().scaledToFit().font( Font.title.weight(.medium)).frame(width: geometry.size.width / 12)
                                 }.padding(.leading, 2.0);
                             

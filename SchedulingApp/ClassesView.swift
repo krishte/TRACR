1e4d4e4dce7581a8ea8ab1479bf0164011a30358
vadrech <charan.vadrevu00@gmail.com>
2020-07-13 15:51:43 +0200

    no line dividers anymore 2

--- SchedulingApp/ClassesView.swift
+++ SchedulingApp/ClassesView.swift
@@ -234,18 +234,6 @@ struct ClassesView: View {
 //
 //    ]
 
-    init() {
-        if #available(iOS 14.0, *) {
-            // iOS 14 doesn't have extra separators below the list by default.
-        } else {
-            // To remove only extra separators below the list:
-            UITableView.appearance().tableFooterView = UIView()
-        }
-
-        // To remove all separators including the actual ones:
-        UITableView.appearance().separatorStyle = .none
-    }
-    
     var body: some View {
          GeometryReader { geometry in
              NavigationView{
--- SchedulingApp/ContentView.swift
+++ SchedulingApp/ContentView.swift
@@ -9,6 +9,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    init() {
+        if #available(iOS 14.0, *) {
+            // iOS 14 doesn't have extra separators below the list by default.
+        } else {
+            // To remove only extra separators below the list:
+            UITableView.appearance().tableFooterView = UIView()
+        }
+
+        // To remove all separators including the actual ones:
+        UITableView.appearance().separatorStyle = .none
+    }
+    
+    
     var body: some View {
         
         TabView {

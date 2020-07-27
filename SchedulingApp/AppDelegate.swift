e486885c8db8ab2c16ed3edc92b3042a17188e46
vadrech <charan.vadrevu00@gmail.com>
2020-07-27 21:28:12 +0200

    locks screen to portrait

--- SchedulingApp/AppDelegate.swift
+++ SchedulingApp/AppDelegate.swift
@@ -13,7 +13,16 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
+    var enableAllOrientation = false
 
+
+    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        if (enableAllOrientation == true){
+            return UIInterfaceOrientationMask.allButUpsideDown
+        }
+        return UIInterfaceOrientationMask.portrait
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true

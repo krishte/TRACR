3aec3edba4b34803e6893b51519eb096e1239d80
Tejas Krishnan <tejas.krishnan81@gmail.com>
2020-07-28 15:54:58 +0200

    nothing really

--- SchedulingApp/ProgressView.swift
+++ SchedulingApp/ProgressView.swift
@@ -246,7 +246,7 @@ struct DetailProgressView: View {
 //                    {
 //                        IndividualAssignemntProgressView(assignment2: assignment)
 //                    }
-//                }
+//                
             }
         }
     }

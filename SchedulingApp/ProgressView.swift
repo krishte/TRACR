8af62dc42361ade8a6c839934a30bf179c5600f6
vadrech <charan.vadrevu00@gmail.com>
2020-07-29 18:11:50 +0200

    sdf

--- SchedulingApp/ProgressView.swift
+++ SchedulingApp/ProgressView.swift
@@ -340,12 +340,12 @@ struct ProgressView: View {
             }
              .navigationBarItems(
                 leading:
-                    HStack(spacing: UIScreen.main.bounds.size.width / 4.2) {
+                HStack(spacing: UIScreen.main.bounds.size.width / 3.7) {
                         Button(action: {print("settings button clicked")}) {
                             Image(systemName: "gear").renderingMode(.original).resizable().scaledToFit().font( Font.title.weight(.medium)).frame(width: UIScreen.main.bounds.size.width / 12)
                         }.padding(.leading, 2.0);
                     
-                        Image("Tracr").resizable().scaledToFit().frame(width: UIScreen.main.bounds.size.width / 4);
+                        Image("Tracr").resizable().scaledToFit().frame(width: UIScreen.main.bounds.size.width / 5);
 
                         Button(action: {
                             self.assignmentlist.count > 0 ? self.NewGradePresenting.toggle() : self.noAssignmentsAlert.toggle()
